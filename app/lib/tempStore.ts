eb9a21eace00e86a4d7db5d6635cc40e2f8d037f
Arlan Marat <arlan@Arlans-MacBook-Air.local>
2024-11-26 12:28:42 -0800

    he

--- app/lib/tempStore.ts
+++ app/lib/tempStore.ts
@@ -1,11 +1,34 @@
-export {};
+interface FormData {
+  message: string;
+  handwriting: { _id: string };
+  card: { _id: string };
+  recipient: {
+    firstName: string;
+    lastName: string;
+    company?: string;
+    street1: string;
+    street2?: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+  from?: {
+    firstName: string;
+    lastName: string;
+    street1: string;
+    street2?: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+}
 
 declare global {
-  var globalTempStore: Map<string, any>;
+  var globalTempStore: Map<string, FormData> | undefined;
 }
 
-if (!global.globalTempStore) {
-  global.globalTempStore = new Map();
+if (typeof global !== 'undefined' && !global.globalTempStore) {
+  global.globalTempStore = new Map<string, FormData>();
 }
 
-export const tempDataStore = global.globalTempStore;
+export const tempDataStore = global.globalTempStore || new Map<string, FormData>();
