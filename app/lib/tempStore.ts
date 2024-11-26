82f442cf5f795503d605bbe3e92a32ab0b140718
Arlan Marat <arlan@Arlans-MacBook-Air.local>
2024-11-26 13:07:20 -0800

    charge fix

--- app/lib/tempStore.ts
+++ app/lib/tempStore.ts
@@ -1,10 +1,10 @@
 // app/lib/tempStore.ts
 declare global {
-  var globalTempStore: Map<string, any>;
+  var globalTempStore: Map<string, any> | undefined;  // Use var here as let/const don't work with global
 }
 
 if (typeof global !== 'undefined' && !global.globalTempStore) {
   global.globalTempStore = new Map<string, any>();
 }
 
-export const tempDataStore = global.globalTempStore || new Map<string, any>();
+export const tempDataStore = (global.globalTempStore as Map<string, any>) || new Map<string, any>();
