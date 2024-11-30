da4eecad070a530073adc7f32d2f1c2b173f090d
Arlan <arlankill12@gmail.com>
2024-11-29 18:02:45 -0800

    metadata add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,34 @@ import type { Metadata } from "next";
 import './globals.css'
 import { consolas, almarenaNeue } from './fonts'
 
-
 export const metadata: Metadata = {
   title: "CardPranks - Anonymous Christmas Cards",
-  description: "Send anonymous prank Christmas cards to your friends and family",
+  description: "Send anonymous prank Christmas cards to your friends and family. Create confusion and laughter with handwritten mystery cards!",
+  keywords: ["prank cards", "anonymous cards", "christmas pranks", "mystery cards", "handwritten cards"],
+  openGraph: {
+    title: 'CardPranks - Anonymous Christmas Cards',
+    description: 'Create confusion and laughter with handwritten mystery cards! Perfect for holiday pranks.',
+    url: 'https://cardpranks.com',
+    siteName: 'CardPranks',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'CardPranks Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CardPranks - Anonymous Christmas Cards',
+    description: 'Create confusion and laughter with handwritten mystery cards! Perfect for holiday pranks.',
+    images: ['/og-image.jpg'],
+  },
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  themeColor: '#000000',
 };
 
 export default function RootLayout({
@@ -17,5 +41,5 @@ export default function RootLayout({
     <html lang="en" className={`${consolas.variable} ${almarenaNeue.variable}`}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
